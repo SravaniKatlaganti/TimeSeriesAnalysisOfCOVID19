c21ffc4f6bb10ea60e25cd90876ff3cf40bd965e
SravaniKatlaganti <113865165+SravaniKatlaganti@users.noreply.github.com>
2022-10-07 18:03:21 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+import numpy as np
+from fastapi import FastAPI, Form
+import pandas as pd
+from starlette.responses import HTMLResponse
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+import re
+
+app = FastAPI()
+
+@app.get('/predict', response_class=HTMLResponse) #data input by forms
+def take_inp():
+
+    return '''
+    <!doctype html>
+<html>
+
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+    <div style="text-align:center;">
+        <h2>Time Series Analysis of COVID 19</h2>
+        <h4>**We help you to forecast COVID new cases**</h4>
+    </div>
+    <form method="post">
+        <div style="width:30%;float: left;">
+            <b>Select Forecast period</b>
+            </br></br>
+            <label style="padding-right: 25px;">From Date: &nbsp;</label>
+            <input type="date" name="fromDate">
+            </br></br>
+            <label style="padding-right: 50px;">To Date: </label>
+            <input type="date" name="toDate">
+            </br></br></br>
+            <div style="padding-left: 50px;">
+                <button (click)="">Generate Forecast Data</button>
+            </div>
+        </div>
+        <div style="width:70%;float: left;">
+            <b>Graph</b>
+        </div>
+    </form>
+</body>
+
+</html>
+    '''
+
+        
+data = pd.read_csv('us-counties-2020.csv')
+# tokenizer = Tokenizer(num_words=2000, split=' ')
+# tokenizer.fit_on_texts(data['text'].values)
+
+def preProcess_data(date):
+    new_date = pd.to_datetime(date, format = '%Y-%m-%d')
+    return new_date
+
+def my_pipeline(start_date,end_date):
+    startDate_new = preProcess_data(start_date)
+    endDate_new = preProcess_data(end_date)
+    # X = tokenizer.texts_to_sequences(pd.Series(text_new).values)
+    # X = pad_sequences(X, maxlen=28)
+    return startDate_new,endDate_new
+
+@app.post('/predict')
+def predict(fromDate:datetime,endDate:datetime = Form(...)):
+    clean_fDate,clean_eDate = my_pipeline(fromDate,endDate) #clean, and preprocess the text through pipeline
+    loaded_model = tf.keras.models.load_model('sentiment.h5') #load the saved model 
+    predictions = loaded_model.predict(clean_fDate) #predict the text
+    sentiment = int(np.argmax(predictions)) #calculate the index of max sentiment
+    probability = max(predictions.tolist()[0]) #calulate the probability
+    if sentiment==0:
+         t_sentiment = 'negative' #set appropriate sentiment
+    elif sentiment==1:
+         t_sentiment = 'neutral'
+    elif sentiment==2:
+         t_sentiment='postive'
+    return { #return the dictionary for endpoint
+         "ACTUALL SENTENCE": clean_eDate,
+         "PREDICTED SENTIMENT": t_sentiment,
+         "Probability": probability
+    }
+
+@app.get('/')
+def basic_view():
+    return {"WELCOME": "GO TO /docs route, or /post or send post request to /predict "}
