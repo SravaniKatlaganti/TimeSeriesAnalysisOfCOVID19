b468d03b0878c41d26c6b11152f19ece90a290a6
SravaniKatlaganti <113865165+SravaniKatlaganti@users.noreply.github.com>
2022-10-15 00:07:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,39 @@ app = FastAPI()
 
 @app.get('/predict', response_class=HTMLResponse) #data input by forms
 def take_inp():
-   return '''<form method="post"> 
-    <input type="number" name="fromDate"/>  
-    <input type="submit"/> 
-    </form>'''   
+  return '''
+    <!doctype html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <div style="text-align:center;">
+        <h2>Time Series Analysis of COVID 19</h2>
+        <h4>**We help you to forecast COVID new cases**</h4>
+    </div>
+    <form method="post">
+        <div style="width:30%;float: left;">
+            <b>Select Forecast period</b>
+            </br></br>
+            <label style="padding-right: 25px;">From Date: &nbsp;</label>
+            <input type="number" name="fromDate">
+            </br></br>
+            </br>
+            <div style="padding-left: 50px;">
+                <input type="submit" value="Generate Forecast">
+            </div>
+            <br><br>
+            <p>[0.27248428, 0.30230462, 0.32341899, 0.34003528, 0.35432757,
+       0.36741912, 0.3798903 , 0.39204094, 0.40402598, 0.41592545,
+       0.42778072, 0.43961315, 0.45143378, 0.46324831, 0.47505968,
+       0.48686944, 0.49867835, 0.51048682, 0.52229507, 0.53410321,
+       0.54591128, 0.55771933, 0.56952736, 0.58133538, 0.59314339]</p>
+        </div>
+    </form>
+</body>
+</html>
+    '''   
 
 @app.get("/graph")
 def image():
