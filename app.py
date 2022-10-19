96dfe06b14cda6a050bf93c2a22fa8cf25332b0d
SravaniKatlaganti <113865165+SravaniKatlaganti@users.noreply.github.com>
2022-10-18 19:55:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import re
+from statsmodels.tsa.arima_model import ARIMAResults
 
 app = FastAPI()
 
@@ -63,30 +64,30 @@ def my_pipeline(start_date):
     return startDate_new
 
 @app.post('/predict',response_class=HTMLResponse)
-def predict():
+def predict(days:int = Form(...)):
     #clean_fDate,clean_eDate = my_pipeline(fromDate) #clean, and preprocess the input through pipeline
-    #loaded_model = tf.keras.models.load_model('Forecast.h5') #load the saved model 
-    #predictions, se, conf = loaded_model.forecast(fromDate) #predict
+    loaded_model = ARIMAResults.load('model.pkl') #load the saved model 
+    predictions = loaded_model.predict(1,days) #predict
     #probability = max(predictions.tolist()[0]) #calulate the probability
-    #return { #return predicted covid cases
-         #"PREDICTED Covid cases for next ": fromDate,
-         #"days are": predictions
-    #}
-    return ''' 
-   <!doctype html>
-<html>
-<head>
-    <title>Home</title>
-</head>
-<body>
-    <h3>Forecast of COVID cases average for the next 25 days :</h3> <br>
-    <p>1) 0.27248428 <br> 2) 0.30230462 <br>3) 0.32341899 <br>4) 0.34003528 <br>5) 0.35432757<br>
-       6) 0.36741912 <br>7) 0.3798903<br> 8) 0.39204094 <br>9) 0.40402598 <br>10) 0.41592545 <br>
-       11) 0.42778072 <br> 12) 0.43961315 <br>13) 0.45143378 <br>14) 0.46324831 <br>15) 0.47505968 <br>
-       16) 0.48686944 <br>17) 0.49867835 <br>18) 0.51048682 <br>19) 0.52229507 <br>20) 0.53410321<br>
-       21) 0.54591128 <br>22) 0.55771933<br> 23) 0.56952736<br> 24) 0.58133538 <br>25) 0.59314339</p>
-   </body>
-</html>'''
+    return { #return predicted covid cases
+         "PREDICTED Covid cases for next ": days,
+         "days are": predictions
+    }
+    #return ''' 
+    #<!doctype html>
+     #<html>
+      # <head>
+       # <title>Home</title>
+       #</head>
+     # <body>
+     # <h3>Forecast of COVID cases average for the next 25 days :</h3> <br>
+     # <p>1) 0.27248428 <br> 2) 0.30230462 <br>3) 0.32341899 <br>4) 0.34003528 <br>5) 0.35432757<br>
+      # 6) 0.36741912 <br>7) 0.3798903<br> 8) 0.39204094 <br>9) 0.40402598 <br>10) 0.41592545 <br>
+       #11) 0.42778072 <br> 12) 0.43961315 <br>13) 0.45143378 <br>14) 0.46324831 <br>15) 0.47505968 <br>
+       #16) 0.48686944 <br>17) 0.49867835 <br>18) 0.51048682 <br>19) 0.52229507 <br>20) 0.53410321<br>
+       #21) 0.54591128 <br>22) 0.55771933<br> 23) 0.56952736<br> 24) 0.58133538 <br>25) 0.59314339</p>
+      #</body>
+    #</html>'''
 
 @app.get('/')
 def basic_view():
